592910f9857f2c93911490dbc6836fe15bf779fe
Murat Başeren <muratbaseren@hotmail.com>
2019-01-14 17:02:40 +0300

    Add project files.

---
+++ MFramework.Services.IdentityAuth/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MFramework.Services.IdentityAuth.MvcApplication" Language="C#" %>
---
+++ MFramework.Services.IdentityAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MFramework.Services.IdentityAuth.Startup))]
+namespace MFramework.Services.IdentityAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
